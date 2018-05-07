afc220f257db7839133f9d2c2b2c85e319c2ba3f
Abbud-Desktop <abbudao@gmail.com>
2018-05-06 23:29:45 -0300

    Fixed gitignore ignoring .java files

---
+++ src/test/java/InvalidGrammarTests.java
@@ -0,0 +1,54 @@
+import java.io.File;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.DisplayName;
+
+public class InvalidGrammarTests{
+    @DisplayName("Invalid Grammar Tests")
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("listTestFiles")
+    public void invalidGrammarTestSuite(String file){
+        InputStream input = System.in;
+        try{
+            input = new FileInputStream(new File(file));
+        }
+        catch (IOException e)
+        {
+            System.out.println("File not found");
+        }
+        boolean encounteredException = false;
+        try{
+            Jason parserInstance = new Jason(input);
+            parserInstance.Program();
+        }
+        catch(ParseException e){
+            encounteredException= true;
+        }
+        finally{
+            if(!encounteredException){
+                fail();
+            }
+        }
+    }
+    private static List<String> listTestFiles(){
+        List<String> results = new ArrayList<String>();
+        String basePath = "src/test/resources/InvalidGrammarTests";
+        File[] files = new File(basePath).listFiles();
+        for (File file : files) {
+            if (file.isFile()) {
+                results.add(basePath+"/"+file.getName());
+            }
+        }
+        return results;
+    }
+}
+
---
+++ src/test/java/ValidGrammarTests.java
@@ -0,0 +1,49 @@
+import java.io.File;
+import java.util.List;
+import java.util.ArrayList;
+import java.io.InputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.DisplayName;
+
+public class ValidGrammarTests{
+    @DisplayName("Valid Grammar Tests")
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("listTestFiles")
+
+    public void ValidGrammarTestSuite(String file){
+        InputStream input = System.in;
+        try{
+            input = new FileInputStream(new File(file));
+        }
+        catch (IOException e)
+        {
+            System.out.println("File not found");
+        }
+        try{
+            Jason parserInstance = new Jason(input);
+            parserInstance.Program();
+        }
+        catch(ParseException e){
+            fail();
+        }
+    }
+
+    private static List<String> listTestFiles(){
+        List<String> results = new ArrayList<String>();
+        String basePath = "src/test/resources/ValidGrammarTests";
+        File[] files = new File(basePath).listFiles();
+        for (File file : files) {
+            if (file.isFile()) {
+                results.add(basePath+"/"+file.getName());
+            }
+        }
+        return results;
+    }
+}
